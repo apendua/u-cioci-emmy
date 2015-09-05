e5c82903355f1916b7e94f55d1251edab9cf6de6
adam.slysz <adam.slysz@grandparade.co.uk>
2015-09-05 10:53:40 +0200

    shop basic skeleton

--- src/emma_shop/urls.py
+++ src/emma_shop/urls.py
@@ -17,8 +17,13 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
 
 urlpatterns = [
+    url(r'^api/', include(router.urls)),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
