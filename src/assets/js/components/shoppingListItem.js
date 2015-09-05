5bf1a3d0835c2f07c960a9cafaa631372398923c
Tomasz Lenarcik <apendua@gmail.com>
2015-09-05 17:44:38 +0200

    Add icons to shopping list items

--- src/assets/js/components/shoppingListItem.js
+++ src/assets/js/components/shoppingListItem.js
@@ -1,5 +1,12 @@
 var Ractive = require('ractive');
+var _ = require('lodash');
 
 Ractive.components.shoppingListItem = Ractive.extend({
-    template: require('./shoppingListItem.html')
+    template: require('./shoppingListItem.html'),
+    data: {
+        icons: function (count) {
+            console.log(count, _.range(count));
+            return _.range(count);
+        },
+    }
 });
