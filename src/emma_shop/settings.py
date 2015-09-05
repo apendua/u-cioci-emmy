20b766ba6011395082e3cefac12667a5a2227cda
Tomasz Lenarcik <apendua@gmail.com>
2015-09-05 11:00:45 +0200

    FIx path for public

--- src/emma_shop/settings.py
+++ src/emma_shop/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'public')
+            os.path.join(BASE_DIR, '../public')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
