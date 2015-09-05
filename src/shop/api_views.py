9c68dae1d0e82890a090ee71be16a8d76de6b85d
gandi <jakub.wasielak@grandparade.co.uk>
2015-09-05 16:40:28 +0200

    Scenario support

--- src/shop/api_views.py
+++ src/shop/api_views.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from django.db.models import Q
 from operator import attrgetter
 import random
 
@@ -10,6 +11,8 @@ from rest_framework.viewsets import ModelViewSet
 
 from shop.models import Item
 from shop.serializers import ItemSerializer
+
+from taggit.models import Tag
 
 
 class ItemViewSet(ModelViewSet):
@@ -35,45 +38,65 @@ def get_game_config(difficulty, level):
 
 
 class GameView(APIView):
-    def get(self, request, format=None, difficulty='easy', level='1'):
-        game_config = get_game_config(difficulty, level)
-        if game_config is None:
-            return HttpResponseServerError(
-                'There is no config for difficulty: {} and level {}'.format(difficulty, level)
-            )
+    def __init__(self, **kwargs):
+        super(GameView, self).__init__(**kwargs)
+        self.items_to_sell = []
+        self.items_to_buy = []
+        self.scenario = None
 
+    def generate_return_dict(self):
+        counts = Counter(attrgetter('name')(item) for item in self.items_to_buy)
+
+        budget = sum(item.price for item in self.items_to_buy) + random.randint(1, 6)
+        self.items_to_buy = list(set(self.items_to_buy))
+        self.items_to_sell = self.items_to_buy + self.items_to_sell
+
+        serialized_shopping_list = ItemSerializer(self.items_to_buy, many=True, context=counts)
+        serialized_available_items = ItemSerializer(self.items_to_sell, many=True, context=counts)
+
+        return {
+            'budget': budget,
+            'scenario': self.scenario,
+            'shopping_list': serialized_shopping_list.data,
+            'available_items': serialized_available_items.data,
+        }
+
+    def generate_scenario(self):
+        categories = Tag.objects.all()
+        category = random.choice(categories)
+
+        self.scenario = category.name
+
+        self.items_to_buy = list(Item.objects.filter(tags__in=[category]))
+        self.items_to_sell = list(Item.objects.filter(~Q(tags__in=[category])))
+
+    def generate_game(self, game_config):
         number_to_sell = game_config['number_of_extra']
         number_to_buy = game_config['number_to_buy']
-
-        items_to_sell = []
-        items_to_buy = []
 
         items = list(Item.objects.all())
         if len(items) < number_to_sell:
             return HttpResponseServerError('Number of items is smaller then the number of items we want to sell')
 
-        while len(items_to_buy) < number_to_buy:
-            items_to_buy.append(weighted_choice(items))
+        while len(self.items_to_buy) < number_to_buy:
+            self.items_to_buy.append(weighted_choice(items))
 
-        counts = Counter(attrgetter('name')(item) for item in items_to_buy)
-
-        while len(items_to_sell) < number_to_sell:
+        while len(self.items_to_sell) < number_to_sell:
             item = random.choice(items)
-            if item not in items_to_sell and item not in items_to_buy:
-                items_to_sell.append(item)
+            if item not in self.items_to_sell and item not in self.items_to_buy:
+                self.items_to_sell.append(item)
             else:
                 items.remove(item)
 
-        budget = sum(item.price for item in items_to_buy) + random.randint(1, 6)
-        items_to_buy = list(set(items_to_buy))
-        items_to_sell = items_to_buy + items_to_sell
-        serialized_shopping_list = ItemSerializer(items_to_buy, many=True, context=counts)
-        serialized_available_items = ItemSerializer(items_to_sell, many=True, context=counts)
+    def get(self, request, format=None, difficulty='easy', level='1'):
+        if difficulty == 'scenario':
+            self.generate_scenario()
+        else:
+            game_config = get_game_config(difficulty, level)
+            if game_config is None:
+                return HttpResponseServerError(
+                    'There is no config for difficulty: {} and level {}'.format(difficulty, level)
+                )
 
-        return_dict = {
-            'budget': budget,
-            'shopping_list': serialized_shopping_list.data,
-            'available_items': serialized_available_items.data,
-        }
-
-        return Response(return_dict)
+            self.generate_game(game_config=game_config)
+        return Response(self.generate_return_dict())
